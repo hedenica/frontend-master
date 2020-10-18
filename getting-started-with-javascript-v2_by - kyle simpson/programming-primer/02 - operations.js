cea7f20c70190b35805f1918292076a7298e97b1
hedenicapedrosa <hedenica@hotmail.com>
2020-10-18 14:02:20 -0300

    ℹ️ operations

---
+++ getting-started-with-javascript-v2_by - kyle simpson/programming-primer/02 - operations.js
@@ -0,0 +1,20 @@
+console.log(3 + 4 )
+
+console.log(43 - 1)
+
+console.log("Hedênica " + "Morais")
+
+//* UNARY operator
+console.log(!false)
+
+//* COMPARISON WITH DOUBLE EQUALS
+console.log(3.0 == 3)
+
+//* BINARY operator
+console.log(3 < 4)
+
+//* LOGICAL operators 'OR' 
+console.log(true || false)
+
+//* LOGICAL operators 'DOUBLE AMPERSAND (and)' 
+console.log(true && false)
