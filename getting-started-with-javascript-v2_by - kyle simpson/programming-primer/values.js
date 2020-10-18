1686ca678c4852f918acd9fb57f5fa5aae79c088
hedenicapedrosa <hedenica@hotmail.com>
2020-10-18 13:50:02 -0300

    ℹ️  values

---
+++ getting-started-with-javascript-v2_by - kyle simpson/programming-primer/values.js
@@ -0,0 +1,21 @@
+//* NUMBERS
+42
+3.14
+
+//* STRING
+"Hello, friend."
+
+//* BOOLEAN
+true
+false
+
+//* EMPTY VALUES 
+null
+undefined
+
+//* ARRAY - ordered collection of values
+[ 1, 2, 3 ]
+
+//* OBJECT - collection of values, with position that you give names for (KEY)
+//{ KEY: "VALUE" }
+{ name: "Hedênica" }
