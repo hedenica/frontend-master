33a8d40e8a95db4c6b3f03a2bb912c443e8589d1
hedenicapedrosa <hedenica@hotmail.com>
2020-10-20 21:37:21 -0300

    if and else

---
+++ getting-started-with-javascript-v2_by - kyle simpson/programming-primer/06 - if_&_else.js
@@ -0,0 +1,16 @@
+//DECISION 
+
+// DO ONE THING DEPENDING ON THE RESULT
+var age = 39;
+
+if (age >= 18 ) {
+  goVote();
+}
+
+// DO ONE THING OR ANOTHER 
+
+if (isEnrolled()) {
+  takeClass();
+} else {
+  enrollFirst();
+}
